42ab123d8032953beadb8cc0929c3d51518cd9cf
DegreesK0 <kelvinbora@gmail.com>
2023-09-16 13:52:08 +0000

    So ctrl+c, exit and enter should now all work as expected

--- 1-ctrl_c_handler.c
+++ 1-ctrl_c_handler.c
@@ -5,11 +5,11 @@
  * @signum: the signal number input
  */
 
-void ctrl_c_handler1(int signum)
+void ctrl_c_handler(int signum)
 {
 	(void) signum;
-	signal(SIGINT, ctrl_c_handler1);
-	printf("\n cannot terminate \n");
+	/* signal(SIGINT, ctrl_c_handler1); */
+	printf("\n($) ");
 	fflush(stdout);
 	/* exit(0); */
 }
--- main.c
+++ main.c
@@ -1,21 +1,6 @@
 #include "bkshell.h"
 
 /**
- * ctrl_c_handler - executes when ctrl+c is pressed
- * @signum: the signal number input
- */
-
-void ctrl_c_handler(int signum)
-{
-	(void) signum;
-
-	printf("\nCtrl+C received. To terminate, type 'exit'.\n");
-	fflush(stdout);
-	/* exit(0); */
-}
-
-
-/**
  * main - runs a simple shell
  * @argc: argument count
  * @argv: argument values
@@ -37,13 +22,13 @@ int main(int argc, char **argv)
 
 	while (1)
 	{
-		/* signal(SIGINT, ctrl_c_handler); */
 		print_prompt();
 		read_input = getline(&lineptr, &n, stdin);
 		/* Allows Ctrl + D to exit on read_input fail*/
 		if (read_input == -1)
 		{
 			free(lineptr);
+			printf("\n");
 			return (-1);
 		}
 		if (read_input == 1) /* Allows enter to work */
@@ -60,8 +45,6 @@ int main(int argc, char **argv)
 		execute_commands(argv);
 		free_array(argv);
 	}
-
 	free(lineptr);
-
 	return (0);
 }
