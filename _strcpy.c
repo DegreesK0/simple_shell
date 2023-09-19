5786ec05241d19cbd6924b21a266f7ddc7163307
root <root@Bennett>
2023-09-19 17:08:11 +0000

    correction to _strcpy.c

--- _strcpy.c
+++ _strcpy.c
@@ -1,7 +1,9 @@
 #include "shell.h"
 
 /**
- *
+ *_strcpy - copis the elements of a given string
+ *@destination: destination of copied string
+ *@source: string to be copied
  */
 
 void _strcpy(char *destination, const char *source)
