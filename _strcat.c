e588c55660552da21de91a6a4719bbc4f4f75ad6
root <root@Bennett>
2023-09-19 15:21:42 +0000

    betty correction for _getenv.c and _strcat.c files

--- _strcat.c
+++ _strcat.c
@@ -1,7 +1,10 @@
 #include "shell.h"
 
 /**
- *
+ * _strcat - concatenates a string to a different location
+ * @destination: destination for the cat string
+ * @source: source string
+ * Return: resulting string
  */
 
 char *_strcat(char *destination, const char *source)
