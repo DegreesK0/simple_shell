0bc10e33eea895359de349df2d150ec1b4748b8d
DegreesK0 <kelvinbora@gmail.com>
2023-09-20 04:41:23 +0000

    more testing

--- u/5main.c
+++ u/5main.c
@@ -6,10 +6,10 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-/* #define BUFFER_SIZE 1024 */
 
-/* void execute_command(const char *command); */
-int exec_command(char *command, ssize_t read_input, char **argv, char **env);
+
+
+void exec_command(char *lineptr, ssize_t read_input, char **argv, char **env);
 
 /**
  * main - Entry point of the shell program.
@@ -19,10 +19,8 @@ int exec_command(char *command, ssize_t read_input, char **argv, char **env);
  *
  * Return: Always returns 0.
  */
-
 int main(int argc, char **argv, char **env)
 {
-	/* char input[BUFFER_SIZE]; */
 	ssize_t read_input;
 	size_t n = 0;
 	char *lineptr = NULL;
@@ -33,14 +31,16 @@ int main(int argc, char **argv, char **env)
 	while (1)
 	{
 		print_string("($) ");
+		/* fgets(input, BUFFER_SIZE, stdin); */
 		read_input = getline(&lineptr, &n, stdin);
 
+		lineptr[strlen(lineptr) - 1] = '\0';
 
-		if (strcmp(lineptr, "exit\n") == 0)
+		if (strcmp(lineptr, "exit") == 0)
 		{
+			/* printf("Exiting the shell...\n"); */
 			break;
 		}
-
 		exec_command(lineptr, read_input, argv, env);
 	}
 
@@ -56,49 +56,49 @@ int main(int argc, char **argv, char **env)
  * @env: environment
  *
  * Description: Forks a child process to execute the commandand waits for it to
- * complete
- *
- * Return: 0 on success
+ * complete.
  */
-
 /* void execute_command(const char *command) */
-int exec_command(char *lineptr, ssize_t read_input, char **args, char **env)
+void exec_command(char *lineptr, ssize_t read_input, char **args, char **env)
 {
-	pid_t pid = fork();
-
+	pid_t pid_child = fork();
+	/* char *executable, *full_path, *full_command; */
+	/* char *env[] = {NULL}; */
 
 	char *executable = NULL;
 	char *actual_command = NULL;
 	char *env_str = "env";
 
-	if (pid == -1)
+	if (pid_child == -1)
 	{
 		perror("Fork failed");
 	}
-	else if (pid == 0)
+	else if (pid_child == 0)
 	{
 		args = tokenize_input(lineptr, read_input, args);
-
 		if (_strcmp(args[0], env_str) == 0)
 		{
 			print_env(env);
+			/* continue; */
 		}
+
+
 		executable = args[0];
 		actual_command = path_finder(executable);
+		/* Execute the actual commands with execve */
+		/* if (execsp(command, argv) == -1) */
 		if (execve(actual_command, args, NULL) == -1)
 		{
 			perror("Exec Error");
 			exit(EXIT_FAILURE);
 		}
-
 		free_array(args);
 	}
 	else
 	{
+		/* Parent process */
 		int status;
 
-		waitpid(pid, &status, 0);
+		waitpid(pid_child, &status, 0);
 	}
-
-	return (0);
 }
